07ea7bdf6619d2507ee8b8cba030807a3632706e
Alumno ULSA <alumno@D515.local>
2022-11-07 13:59:27 -0700

    avance

---
+++ proyecto3Parcial/CeldaComidaController.swift
@@ -0,0 +1,13 @@
+//
+//  CeldaComidaController.swift
+//  proyecto3Parcial
+//
+//  Created by Alumno ULSA on 07/11/22.
+//  Copyright © 2022 Alumno. All rights reserved.
+//
+
+import UIKit
+
+class CeldaComidaController: UICollectionViewCell  {
+    
+}
